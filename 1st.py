38a8b0ac369112d6aca63169bde455aa2f2b1e13
Sher Bano <anjum.uber846@gmail.com>
2026-09-22 19:57:59 +0500

    my first cmnt
    Eshall

---
+++ 1st.py
@@ -0,0 +1,25 @@
+# a=1
+# a=1.1
+a=complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print(a)
+a1 = 9
+# Harry=9
+# b = "Harry"
+print(b)
+print(a+a1)
+print("The type of a is ",   type(a))
+print("The type of b is ",   type(b))
+print("The type of c is ",   type(c))
+list1 = [8, 2.3, [-4, 5], ["apple", 
+"banana"]]
+print(list1)
+# list are  change but tuple are not change
+tuple1 = (("parrot", "Sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+#Mapped data:dict
+dict1 = {"name":"Esha","age":20,
+"canVote":True}
+print(dict1)
---
+++ 2nd.py
@@ -0,0 +1,11 @@
+'''
+(multi line cmnt)/("""""")/('''''')
+print("Hello world")
+# This is a single line coomint
+'''
+# hey harry,please don't remove this line
+# Author:harry (ctrl+/)
+# course:100 days of code
+print('Hey i am a\' \"good boy\"\nand this viewer i also a good boy/girl')
+print("hey", 6, 7, sep="~", end="009")
+print("harry")
